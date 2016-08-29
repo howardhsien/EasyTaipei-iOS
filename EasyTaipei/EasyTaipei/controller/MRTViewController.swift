9dac87b009696c7b34086c7498f74ebb986dab16
wuuhren <wuduhren@outlook.com>
2016-08-29 22:52:44 +0800

    initial commit

--- EasyTaipei/EasyTaipei/controller/MRTViewController.swift
+++ EasyTaipei/EasyTaipei/controller/MRTViewController.swift
@@ -11,32 +11,36 @@ import UIKit
 class MRTViewController: UIViewController,UIScrollViewDelegate {
 //ScrollView image viewer
     
-    lazy var scrollView:UIScrollView = {
+    private var buttonTagSelected = [UIButton]()
+    
+    private lazy var scrollView:UIScrollView = {
         let sv = UIScrollView(frame: self.view.bounds)
         sv.delegate = self
         return sv
     }()
     
-    lazy var imageView:UIImageView = {
-        if let image = UIImage(named: "mrtMap"){
-            let imgView = UIImageView(image: image)
+    private lazy var mrtView: UIView = {
+        //should not use imageView as before; the button added on imageView will not respond.
+        if let mrtView = UIView.loadFromNibNamed("mrtView"){
+            
             let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
             if let tabHeight = self.tabBarController?.tabBar.frame.height,
                 navHeight = self.navigationController?.navigationBar.frame.height
-            
+                
             {
-                imgView.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height  - tabHeight - navHeight - statusBarHeight)
+                mrtView.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height  - tabHeight - navHeight - statusBarHeight)
             }
             else{
-                imgView.frame = self.view.frame
+                mrtView.frame = self.view.frame
             }
-            imgView.contentMode = .ScaleToFill
-            return imgView
+            mrtView.contentMode = .ScaleToFill
+            return mrtView
         }
         else{
-            return UIImageView()
+            return UIView()
         }
     }()
+
     
     //MARK: LifeCycle
     override func viewDidLoad() {
@@ -45,19 +49,20 @@ class MRTViewController: UIViewController,UIScrollViewDelegate {
         setupScrollView()
         setZoomScale()
         setupGestureRecognizer()
+        buttonIteration()
         
     }
     //MARK: ScrollView
     private func setupScrollView(){
-        scrollView.addSubview(imageView)
+        scrollView.addSubview(mrtView)
         scrollView.maximumZoomScale = 4.0
         scrollView.minimumZoomScale = 1.0
-        scrollView.contentSize = imageView.frame.size
+        scrollView.contentSize = mrtView.frame.size
     }
     
     //handle the zooming
     private func setZoomScale() {
-        let imageViewSize = imageView.bounds.size
+        let imageViewSize = mrtView.bounds.size
         let scrollViewSize = scrollView.bounds.size
         let widthScale = scrollViewSize.width / imageViewSize.width
         let heightScale = scrollViewSize.height / imageViewSize.height
@@ -70,6 +75,11 @@ class MRTViewController: UIViewController,UIScrollViewDelegate {
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
         doubleTap.numberOfTapsRequired = 2
         scrollView.addGestureRecognizer(doubleTap)
+        
+        //oneTap for location
+        let oneTap = UITapGestureRecognizer(target: self, action: #selector(oneTapAction))
+        oneTap.numberOfTapsRequired = 1
+        scrollView.addGestureRecognizer(oneTap)
     }
     
     @objc private func handleDoubleTap(recognizer: UITapGestureRecognizer) {
@@ -89,14 +99,14 @@ class MRTViewController: UIViewController,UIScrollViewDelegate {
     
     
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        return imageView
+        return mrtView
     }
 
 }
 
 extension UIScrollView {
     
-    func zoomToPoint( zoomPoint: CGPoint, withScale scale:CGFloat, animated :Bool)
+    private func zoomToPoint( zoomPoint: CGPoint, withScale scale:CGFloat, animated :Bool)
     {
         //Normalize current content size back to content scale of 1.0f
         var contentSize = CGSize()
@@ -124,3 +134,77 @@ extension UIScrollView {
         zoomToRect(zoomRect, animated: animated)
     }
 }
+
+
+
+
+//MARK: Button
+extension MRTViewController {
+    
+    //add button
+    func buttonIteration(){
+        
+        let stationLocation = [
+            "中山":"{164.0, 317.666656494141}",
+            "雙連":"{163.666656494141, 294.0}",
+            "民權西路":"{163.666656494141, 271.33332824707}",
+            "圓山":"{163.0, 246.666656494141}",
+            "劍潭":"{163.0, 224.0}",
+            "士林":"{162.666656494141, 204.666656494141}",
+            "芝山":"{163.33332824707, 185.33332824707}",
+            "明德":"{163.33332824707, 163.33332824707}"
+        ]
+        
+        
+        
+        for (station, coordinate) in stationLocation {
+            let button = UIButton(type: .System)
+            button.center = CGPointFromString(coordinate)
+            button.bounds = CGRect(x: 0,y: 0,width: 9,height: 9)
+            button.backgroundColor = .redColor()
+            button.alpha = 0.6
+            button.addTarget(self, action: #selector(MRTViewController.EstimatedArrivalTimer(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+            
+            button.setTitle(station, forState: UIControlState.Normal)
+            button.tintColor = .clearColor()
+            
+            mrtView.addSubview(button)
+        }
+    }
+    
+    //show coordinates of stations
+    func oneTapAction(recognizer:UITapGestureRecognizer){
+        //Log Clicked Location
+        let tapPoint = recognizer.locationInView(mrtView)
+        print(tapPoint)
+        cleanButton()
+    }
+    
+    func EstimatedArrivalTimer(sender: UIButton!){
+        print("EstimatedArrivalTimer")
+        sender.backgroundColor = .greenColor()
+        
+        if buttonTagSelected.count < 2 {
+            buttonTagSelected.append(sender)
+            
+        } else {
+            cleanButton()
+            buttonTagSelected.append(sender)
+        }
+        
+        if buttonTagSelected.count == 2 {
+            showEstimatedArrivalTime()
+        }
+    }
+    
+    func showEstimatedArrivalTime() {
+    }
+    
+    func cleanButton() {
+        for button in buttonTagSelected {
+            button.backgroundColor = .redColor()
+        }
+        buttonTagSelected = []
+    }
+}
+
---
+++ EasyTaipei/EasyTaipei/view/mrtView.swift
@@ -0,0 +1,21 @@
+//
+//  mrtView.swift
+//  EasyTaipei
+//
+//  Created by Eph on 2016/7/15.
+//  Copyright © 2016年 AppWorks School Hsien. All rights reserved.
+//
+
+import UIKit
+
+class mrtView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
